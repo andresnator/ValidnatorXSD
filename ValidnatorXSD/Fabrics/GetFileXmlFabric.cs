8829233cb8de4bf5ed654162ed48e8ed49a79a06
jgonzalg <jgonzalg@JRROMERO2>
2018-06-21 17:27:57 -0500

    Add Fabric Types FIles

---
+++ ValidnatorXSD/Fabrics/GetFileXmlFabric.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml.Linq;
+using ValidenatorXSD.Const;
+using ValidenatorXSD.IC;
+
+namespace ValidenatorXSD.Fabrics
+{
+    public class GetFileXmlFabric : IGetFileXml
+    {
+        private readonly IGetFileXml instance;
+
+        public GetFileXmlFabric()
+        {
+        }
+
+        public GetFileXmlFabric(EnumsValidatorXSD.TypeFile typeFile)
+        {
+            switch (typeFile)
+            {
+                case EnumsValidatorXSD.TypeFile.Xls:
+                    break;
+                case EnumsValidatorXSD.TypeFile.Xlsx:
+                    break;
+                case EnumsValidatorXSD.TypeFile.Csv:
+                    break;
+                case EnumsValidatorXSD.TypeFile.Txt:
+                    instance = new GetFileXmlTxt();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(typeFile), typeFile, null);
+            }
+        }
+
+        public XElement ConvertFileXml(string urlFile)
+        {
+            return instance.ConvertFileXml(urlFile);
+        }
+    }
+}
---
+++ ValidnatorXSD/Fabrics/GetFileXmlTxt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Xml.Linq;
+using ValidenatorXSD.IC;
+
+namespace ValidenatorXSD
+{
+    internal class GetFileXmlTxt : IGetFileXml
+    {
+        public XElement ConvertFileXml(string urlFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
